b95e8857687a6ec2ce6a56dac7b06060c34d6d98
KendraG23 <shaw.kendra23@gmail.com>
2024-09-27 15:49:21 -0400

    First Commit - Snake Game

---
+++ gameSnake/src/Snake/Snake/SnakeGame.java
@@ -0,0 +1,10 @@
+package Snake;
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new GameFrame(); //GameFrame frame = new GameFrame() creates instances of GameFreame
+	}
+
+}
